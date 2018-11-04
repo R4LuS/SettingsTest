57689dc0658f46bb170d07240b30a29006b564b7
R4LuS <ralus794@gmail.com>
2018-11-04 16:14:14 +0100

    Main

--- SettingsTest/main.cpp
+++ SettingsTest/main.cpp
@@ -1,8 +1,20 @@
 #include <QtCore/QCoreApplication>
+#include <QSettings>
+
+void save();
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
-
+	save();
 	return a.exec();
+}
+
+
+void save() {
+	QSettings settings("settings.ini", QSettings::IniFormat);
+	settings.beginGroup("BuriedCO");
+	settings.setValue("IP", "192.168.1.125");
+	settings.setValue("Port", 8056);
+	settings.endGroup();
 }
